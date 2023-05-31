08a57f9bcb5d7bb325bf87f4c5a4ccf1ac7d0044
JyunKai <www10255043@gmail.com>
2023-05-31 16:36:09 +0800

    feat: 讀取使用者資料

--- lib/provider/abilities_provider.dart
+++ lib/provider/abilities_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rpg/entity/abilities_entity.dart';
 import 'package:rpg/utility/role_data.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final abilitiesProvider = StateNotifierProvider<AbilitiesNotifier, AbilitiesEntity>((ref) => AbilitiesNotifier());
 
@@ -12,5 +13,11 @@ class AbilitiesNotifier extends StateNotifier<AbilitiesEntity> {
     AbilitiesEntity newCharacter = AbilitiesEntity();
     newCharacter = (option[0] == 0 && option[1] == 3 && option[2] == 3) ? gmAbility : adventurerAbility;
     Future.delayed(const Duration(seconds: 3), () => state = newCharacter);
+  }
+
+  //獲取當前角色數據
+  void getAbility() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    state = abilitiesEntityFromJson(prefs.getString('ability')!);
   }
 }
