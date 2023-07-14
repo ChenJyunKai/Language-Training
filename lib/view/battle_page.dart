8e1b539e80db804da8656fc56a89856b70f2757a
JyunKai <www10255043@gmail.com>
2023-07-14 15:03:58 +0800

    add: animation_elf.json feat: 遊戲模式選擇

--- lib/view/battle_page.dart
+++ lib/view/battle_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
+import 'package:rpg/entity/word_jp_entity.dart';
+import 'package:rpg/helper/screen_size.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 
@@ -9,26 +13,44 @@ class BattlePage extends StatefulWidget {
   State<BattlePage> createState() => _BattlePageState();
 }
 
-class _BattlePageState extends State<BattlePage> {
+class _BattlePageState extends State<BattlePage> with SingleTickerProviderStateMixin {
   final SpeechToText _speechToText = SpeechToText();
   bool _speechEnabled = false;
   String _lastWords = '';
-
+  late List<Word> words;
+  static const List<String> language = ['日語', '英語', '中文'];
+  late AnimationController animationController;
 
   @override
   void initState() {
     super.initState();
+    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
     _initSpeech();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 
   void _initSpeech() async {
     _speechEnabled = await _speechToText.initialize();
+    words = wordJpEntityFromJson(await rootBundle.loadString('assets/json/word_jp.json')).words;
     setState(() {});
   }
 
-  void _startListening() async {
-    // en_US 、 ja_JA
-    await _speechToText.listen(onResult: _onSpeechResult,localeId: 'ja_JA');
+  void _startListening(String language) async {
+    String localeId = 'zh_TW';
+    switch (language) {
+      case '日語':
+        localeId = 'ja_JA';
+      case '英語':
+        localeId = 'en_US';
+      case '中文':
+        localeId = 'zh_TW';
+    }
+    await _speechToText.listen(onResult: _onSpeechResult, localeId: localeId);
     setState(() {});
   }
 
@@ -48,38 +70,123 @@ class _BattlePageState extends State<BattlePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Speech Demo'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: const Text(
-                'Recognized words:',
-                style: TextStyle(fontSize: 20.0),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                  _speechToText.isListening
-                      ? _lastWords
-                      : _speechEnabled
-                          ? 'Tap the microphone to start listening...'
-                          : 'Speech not available',
-                ),
-              ),
-            ),
+      body: ClipRect(
+        child: Stack(
+          children: [
+            selectLanguage(),
+            rule(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _speechToText.isNotListening ? _startListening : _stopListening,
-        tooltip: 'Listen',
-        child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _speechToText.isNotListening ? _startListening : _stopListening,
+      //   tooltip: 'Listen',
+      //   child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
+      // ),
+    );
+  }
+
+  Widget selectLanguage() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SlideTransition(
+          position: Tween(begin: const Offset(0, 0), end: const Offset(-4, 0)).animate(CurvedAnimation(
+            parent: animationController,
+            curve: const Interval(0, 1, curve: Curves.fastOutSlowIn),
+          )),
+          child: Container(
+            width: screenWidth,
+            alignment: Alignment.center,
+            child: const Text(
+              "請選擇語言模式",
+              style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Container(
+          height: screenHeight * 0.3,
+          padding: const EdgeInsets.only(top: 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              for (String i in language)
+                SlideTransition(
+                  position: Tween(begin: const Offset(0, 0), end: const Offset(-4, 0)).animate(CurvedAnimation(
+                    parent: animationController,
+                    curve: Interval(0.1 * language.indexWhere((e) => e == i), 1, curve: Curves.fastOutSlowIn),
+                  )),
+                  child: SizedBox(
+                    height: 50,
+                    width: screenWidth * 0.7,
+                    child: ElevatedButton(
+                      onPressed: () => animationController.animateTo(1),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white70,
+                      ),
+                      child: Text(i, style: const TextStyle(fontSize: 20)),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget rule() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SlideTransition(
+          position: Tween(begin: const Offset(2, 0), end: const Offset(0, 0)).animate(CurvedAnimation(
+            parent: animationController,
+            curve: const Interval(0, 1, curve: Curves.fastOutSlowIn),
+          )),
+          child: Lottie.asset('assets/lottie/animation_elf.json',height: 200),
+        ),
+        SlideTransition(
+          position: Tween(begin: const Offset(3, 0), end: const Offset(0, 0)).animate(CurvedAnimation(
+            parent: animationController,
+            curve: const Interval(0, 1, curve: Curves.fastOutSlowIn),
+          )),
+          child: Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: screenWidth * 0.6,
+              child: const Text(
+                "請根據上面的單字唸出讀音",
+                style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ),
+        SlideTransition(
+          position: Tween(begin: const Offset(4, 0), end: const Offset(0, 0)).animate(CurvedAnimation(
+            parent: animationController,
+            curve: const Interval(0.5, 1, curve: Curves.fastOutSlowIn),
+          )),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: const Color(0xff132137),
+                ),
+                child: const Text("Let's begin", style: TextStyle(fontSize: 18)),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
