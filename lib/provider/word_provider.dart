0eb5cdf1a9ef88cb1c9526df02812d330ec6b1c6
JyunKai <www10255043@gmail.com>
2023-07-17 16:35:29 +0800

    feat: word 隨機抽取

--- lib/provider/word_provider.dart
+++ lib/provider/word_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rpg/entity/word_entity.dart';
@@ -8,7 +10,12 @@ class WordNotifier extends StateNotifier<WordEntity> {
   WordNotifier() : super(WordEntity(words: [], languageId: 'ja_JA'));
 
   void getData(String languageId) async {
-    state = wordEntityFromJson(await rootBundle.loadString('assets/json/word_jp.json'));
+    final data = wordEntityFromJson(await rootBundle.loadString('assets/json/word_jp.json'));
+    final words = <Word>{};
+    while (words.length < 10) {
+      words.add(data.words[Random().nextInt(data.words.length)]);
+    }
+    state = WordEntity(words: words.toList(), languageId: languageId);
   }
 
   void remove(String word) async {
