85f11c71fddec858402f7647593e6b959eb96823
JyunKai <www10255043@gmail.com>
2023-07-31 13:51:25 +0800

    chore: windows settings

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,8 +25,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
 
   FlutterWindow window(project);
-  Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Point origin(1200, 100);
+  Win32Window::Size size(460, 720);
   if (!window.Create(L"rpg", origin, size)) {
     return EXIT_FAILURE;
   }
